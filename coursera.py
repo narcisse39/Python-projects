d5c4315e22b0bc0a7443fd7144a5c3c4c1fda542
narcisse39 <narcisse352@gmail.com>
2020-10-01 18:40:42 +0200

    git commit

--- coursera.py
+++ coursera.py
@@ -1,34 +1,84 @@
-fname = 'mbox-short.txt'
-try:
-    fhand = open(fname)
-    counts = dict()
-    lst_item = list()
-except:
-    print('File cannot be opened:', fname)
-    exit()
+"""
+from pymongo import MongoClient
+from random import randint
+#Step 1: Connect to MongoDB - Note: Change connection string as needed
+client = MongoClient(port=27017)
+db=client.business
+#Step 2: Create sample data
+names = ['Kitchen','Animal','State', 'Tastey', 'Big','City','Fish', 'Pizza','Goat', 'Salty','Sandwich','Lazy', 'Fun']
+company_type = ['LLC','Inc','Company','Corporation']
+company_cuisine = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
+for x in range(1, 501):
+    business = {
+        'name' : names[randint(0, (len(names)-1))] + ' ' + names[randint(0, (len(names)-1))]  + ' ' + company_type[randint(0, (len(company_type)-1))],
+        'rating' : randint(1, 5),
+        'cuisine' : company_cuisine[randint(0, (len(company_cuisine)-1))] 
+    }
+    #Step 3: Insert business object directly into MongoDB via isnert_one
+    result=db.reviews.insert_one(business)
+    #Step 4: Print to the console the ObjectID of the new document
+    print('Created {0} of 500 as {1}'.format(x,result.inserted_id))
+#Step 5: Tell us that you are done
+print('finished creating 500 business reviews')
+"""
+import mysql.connector
+from mysql.connector import Error
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
-for line in fhand:
-    line = line.rstrip()
-    if not line.startswith('From '):
-        continue
-    words = line.split()
-    _hour = words[5].split(':')
-    counts[_hour[0]] = counts.get(_hour[0], 0) + 1
+    return connection
 
-print(counts)
 
-for key,val in counts.items():
-	newtup = (val,key)
-	lst_item.append(newtup)
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
-print('='*60)
-print('Flipped\n',lst_item)
-
-print('='*60)
-lst_item = sorted(lst_item)
-print('Sorted\n',lst_item)
-
-print('='*60)
-for value,key in lst_item:
-	print(key,value)
-
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT, 
+  name TEXT NOT NULL, 
+  age INT, 
+  gender TEXT, 
+  nationality TEXT, 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+  id INT AUTO_INCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id), 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+create_users = """
+INSERT INTO
+  `users` (`name`, `age`, `gender`, `nationality`)
+VALUES
+  ('James', 25, 'male', 'USA'),
+  ('Leila', 32, 'female', 'France'),
+  ('Brigitte', 35, 'female', 'England'),
+  ('Mike', 40, 'male', 'Denmark'),
+  ('Elizabeth', 21, 'female', 'Canada');
+"""
+connection = create_connection("localhost", "root", "Tande@2020", "sm_app")
+execute_query(connection, create_users_table)
+execute_query(connection, create_posts_table)
+execute_query(connection, create_users)  
